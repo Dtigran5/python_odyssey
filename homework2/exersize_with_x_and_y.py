5c4d62d7c546c5e65075089d89ce8a0cc33e9814
Dtigran5 <138877144+Dtigran5@users.noreply.github.com>
2024-05-31 17:48:42 +0400

    Add files via upload

---
+++ homework2/exersize_with_x_and_y.py
@@ -0,0 +1,5 @@
+x = float(input("Enter the value of x: ")) 
+y = float(input("Enter the value of y: "))
+
+result = (x - y) / (x + y) 
+print("Result: ", result)
---
+++ homework2/kilogram_to_pound.py
@@ -0,0 +1,4 @@
+weight_kg = float(input("Enter weight in kilograms: "))
+weight_pound = weight_kg * 2.2
+
+print(f"{weight_kg} kilograms is equal to {weight_pound} pounds.")
