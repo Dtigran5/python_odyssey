fb8197df8bc250acaf58a276b0fb24b16062f602
Dtigran5 <138877144+Dtigran5@users.noreply.github.com>
2024-07-05 17:49:24 +0400

    Add files via upload

---
+++ homework12/set_union_intersection_symetric_difference.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print("Union:", set1.union(set2))
+print("Intersection:", set1.intersection(set2))
+print("Symmetric Difference:", set1.symmetric_difference(set2))
